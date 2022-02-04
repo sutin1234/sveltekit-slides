63e0763169c4d7ffe2138bbb4955586222a7c647
Sutin <poly.dev2018@gmail.com>
2022-02-05 02:23:15 +0700

    added seo title

---
+++ src/routes/blogs/[slug].svelte
@@ -0,0 +1,22 @@
+<script context="module">
+	export const load = async ({ params }) => {
+		const { slug } = params;
+		return {
+			props: {
+				slug
+			}
+		};
+	};
+</script>
+
+<script>
+	import { seo, update } from '$lib/store/seo';
+	export let slug;
+	$: slug_title = slug.replace(/-/g, ' ');
+	$seo.title = slug_title;
+</script>
+
+<!-- <svelte:head><title>{'Blog: ' + slug_title}</title></svelte:head> -->
+<div class="flex justify-center items-center bg-white m-4">
+	{$seo.title}
+</div>
