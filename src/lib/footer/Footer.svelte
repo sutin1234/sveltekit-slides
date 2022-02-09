7befe0f52aa2b9f10cd9c820bdc4785751dbf31d
Sutin <poly.dev2018@gmail.com>
2022-02-09 14:59:53 +0700

    fixed bottom

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,5 +1,5 @@
 <div
-	class="w-full p-4 text-center bg-gray-500 h-40 text-white flex flex-wrap justify-center items-center bottom-0"
+	class="w-full p-4 text-center bg-gray-500 h-40 text-white flex flex-wrap justify-center fixed items-center bottom-0"
 >
 	<div class="p-8">Power By Sveltekit @2022</div>
 	<div class="p-8">Power By Sveltekit @2022</div>
