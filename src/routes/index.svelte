9dc5001fc2cbd4e820ef3a9f86f2ba35dc6df329
Sutin <poly.dev2018@gmail.com>
2022-03-02 09:14:57 +0700

    added target

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,9 @@
 </script> -->
 <script>
 	import { seo } from '$lib/store/seo';
-	import Highlight from 'svelte-highlight';
+	// import Highlight from 'svelte-highlight';
 	import { HighlightAuto } from 'svelte-highlight';
-	import typescript from 'svelte-highlight/src/languages/typescript';
+	// import typescript from 'svelte-highlight/src/languages/typescript';
 	// import github from 'svelte-highlight/src/styles/github';
 	import 'svelte-highlight/src/styles/atom-one-dark.css';
 
@@ -37,4 +37,4 @@
 	<HighlightAuto {code} />
 </div>
 
-<a href="http://projanco.com/Library/AngularJS%20in%20Action.pdf">download pdf</a>
+<a href="http://projanco.com/Library/AngularJS%20in%20Action.pdf" target="_system">download pdf</a>
